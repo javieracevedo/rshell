447d23b9ca59793fc8da16d0ae771c09988f859b
javieracevedo <javier.acevedo233@gmail.com>
2025-07-10 18:27:07 -0400

    Update shell to use raw mode, so we can intercept special characters properly.

--- rshell.rb
+++ rshell.rb
@@ -1,3 +1,5 @@
+require 'io/console'
+
 module RShell
     require_relative "built-ins"
     require_relative "config"
@@ -5,6 +7,9 @@ module RShell
     CD_CMD = "cd"
     EXIT_CMD = "exit"
     PATH_CMD = "path"
+
+    REDIRECT_TRUNCATE_TOKEN = ">"
+    REDIRECT_APPEND_TOKEN = ">>"
 
     def self.display_prompt()
         rgb_val = ShellConfig::RGB_COLOR_MAP[:red]
@@ -17,35 +22,89 @@ module RShell
         puts ""
     end
 
-    def self.execute_command(args)
-        begin
-            pid = spawn({ 'PATH' => ShellConfig.path.join(":")}, *args)
-            Process.wait(pid)
-            status = $?.exitstatus
-            puts "Exited with status #{status}" unless status == 0
-        rescue Errno::ENOENT
-            puts "Command not found: #{args[0]}"
-        rescue SystemCallError => e
-            puts "System error: #{e.message}"
+    def self.execute_command(args, output_file_path, should_truncate)
+        mode = File::WRONLY | File::CREAT
+        mode |= should_truncate ? File::TRUNC : File::APPEND
+
+        out_fd = output_file_path != nil ? File.open(output_file_path, mode, 0644)  : STDOUT
+        err_fd = output_file_path != nil ? out_fd : STDERR
+
+        STDIN.cooked do
+            begin
+                pid = spawn(
+                    { 'PATH' => ShellConfig.path },
+                    *args,
+                    [:out] => out_fd,
+                    [:err] => err_fd
+                )
+                Process.wait(pid)
+                status = $?.exitstatus
+                puts "Exited with status #{status}" unless status == 0
+                rescue Errno::ENOENT
+                    puts "Command not found: #{args[0]}"
+                rescue SystemCallError => e
+                    puts "System error: #{e.message}"
+                ensure
+                    out_fd.close if output_file_path && out_fd && !out_fd.closed?
+            end
+        end
+     end
+
+    def self.start_interactive()
+        input_buffer = ""
+        prompt = "\e[38;2;#{ShellConfig::RGB_COLOR_MAP[:red]}m@rshel #{Dir.getwd()} >> \e[0m"
+
+        STDIN.raw do
+            print prompt
+
+            loop do
+                char = STDIN.getch
+
+                if char == "\u007f"
+                    unless input_buffer.empty?
+                        input_buffer.chop!
+                        print "\b \b"
+                    end
+                elsif char == "\r" || char == "\n"
+                    print "\r\n"
+
+                    output = process_command(input_buffer)
+                    break if output == "exit"
+
+                    input_buffer = ""
+                    print "\r" + prompt
+                else
+                    input_buffer << char
+                    print char
+                end
+            end
         end
     end
 
-    def self.start_interactive()
-        loop do
-            display_prompt()
+    def self.process_command(cmd_line)
+        args = cmd_line.chomp.split
 
-            args = gets.chomp.split
+        if args.length > 0
+            output_file_path = nil
+            truncate_output_file = false
 
-            next if args.length == 0
+            redirect_operand = (args & [REDIRECT_TRUNCATE_TOKEN, REDIRECT_APPEND_TOKEN]).first
+            if redirect_operand != nil
+                redirect_op_index = args.index(redirect_operand)
+                output_file_path = args[redirect_op_index+1]
+                args = args.slice(0, redirect_op_index)
+                truncate_output_file = redirect_operand == REDIRECT_TRUNCATE_TOKEN
+            end
+
 
             if args[0] == EXIT_CMD
-                break
+                return "exit"
             elsif args[0] == CD_CMD
                 BuiltIn.cd(args)
             elsif args[0] == PATH_CMD
                 BuiltIn.path(args)
             else
-                execute_command(args)
+                execute_command(args, output_file_path, truncate_output_file)
             end
         end
     end
